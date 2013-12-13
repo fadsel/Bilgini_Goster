1dedf99f0b0c490c1c93e11f33879b8bd1501ac7
Fahad Kassim <fahad@yoteyote.com>
2013-12-13 13:21:17 +0200

    Message To the viewers

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
 <?php include('usuals/sideBar.php'); ?>
 
 <div class="row">
-    <div class="jumbotron" id="startHere"><h2><b>Documentation</b> Beyler</h2>Hello world , Opps their isnt one :)</div></div>
+    <div class="jumbotron" id="startHere"><h2><b>Documentation</b></h2>Hello world , Opps their isnt one :), We have to learn bootstrap  inorder to toggle the CSS with different devices <b>Beyler</b> tablet , mobile , desktop & Big machines</div></div>
 
 <?php include('usuals/footer.php'); ?>
