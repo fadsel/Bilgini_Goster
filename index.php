a9433351edf4e19560ae48828f1368e627be337d
Fahad Kassim <fahad@yoteyote.com>
2013-12-13 21:36:36 +0200

    Changed Fonts to Open sans from Google
    Added Search button :) at the buttom of the page ;)

--- index.php
+++ index.php
@@ -10,5 +10,16 @@
         <div class="well col-lg-4" ><h2><b>Once sync yapalim</b></h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
         <div class="well col-lg-4" ><h2><b>Stack Overflow</b></h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
     </div>
+<div class="row">
+    <header class="col-lg-12 title">
+        <h2><b>Didnt Find What you are looking for?</b></h2>
+        <p>You can always search it</p>
+        <div class="search">
+            <input type="text" id="inputProjectSearch" placeholder="What are you looking for?" data-role="search-textbox" data-url="/index.php?search=" value="">
+            <a href="#" class="btn btn-bilgini" data-role="search-button">Search</a>
+        </div>
+    </header>
+
+</div>
 
 <?php include('usuals/footer.php'); ?>
--- usuals/header.php
+++ usuals/header.php
@@ -8,6 +8,7 @@
     <link href="css/bgo-bootstrap-min.css" rel="stylesheet">
     <link href="css/kituChaPembeni.css" rel="stylesheet">
     <link href="css/custom.css" rel="stylesheet">
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,400,300' rel='stylesheet' type='text/css'>
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
