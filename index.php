88229f2f1084deafb3f1e9190626c38923b46183
Fahad Kassim <fahad@yoteyote.com>
2013-12-13 13:28:01 +0200

    Fixed some extra padding

--- index.php
+++ index.php
@@ -5,4 +5,10 @@
 <div class="row">
     <div class="jumbotron" id="startHere"><h2><b>Documentation</b></h2>Hello world , Opps their isnt one :), We have to learn bootstrap  inorder to toggle the CSS with different devices <b>Beyler</b> tablet , mobile , desktop & Big machines</div></div>
 
+    <div class="row">
+        <div class="well col-lg-4" ><h2><b>Plagiarism</b></h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+        <div class="well col-lg-4" ><h2><b>Once sync yapalim</b></h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+        <div class="well col-lg-4" ><h2><b>Stack Overflow</b></h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+    </div>
+
 <?php include('usuals/footer.php'); ?>
