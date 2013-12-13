5cb9f853a00a41239a0f6e86fa31dc18dc111d1e
Fahad Kassim <fahad@yoteyote.com>
2013-12-13 21:40:50 +0200

    Separated search from index

---
+++ usuals/search.php
@@ -0,0 +1,11 @@
+<div class="row">
+    <header class="col-lg-12 title">
+        <h2><b>Didnt Find What you are looking for?</b></h2>
+        <p>You can always search it</p>
+        <div class="search">
+            <input type="text" id="inputProjectSearch" placeholder="What are you looking for?" data-role="search-textbox" data-url="/index.php?search=" value="">
+            <a href="#" class="btn btn-bilgini" data-role="search-button">Search</a>
+        </div>
+    </header>
+
+</div>
