3537ec574095511f7d31ba2f0f5f6c7d056863bd
Fahad Kassim <fahad@yoteyote.com>
2013-12-17 01:37:40 +0200

    Fixed the slider
    Removed extra stuff , added a few paddings at the bottom

--- modules/slider.php
+++ modules/slider.php
@@ -1,6 +1,6 @@
 <div class="row">
     <div class="">
-<div id="carousel-example-generic" class="carousel slide ">
+<div id="carousel-example-generic" class="carousel slide" style="margin-bottom: 35px">
     <!-- Indicators -->
     <ol class="carousel-indicators">
         <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
@@ -13,13 +13,13 @@
         <div class="item active">
             <img src="./img/test/callout3.jpg" alt="...">
             <div class="carousel-caption">
-                ...
+                Wazushi
             </div>
         </div>
         <div class="item">
             <img src="./img/test/callout2.jpg" alt="...">
             <div class="carousel-caption">
-                ...
+               Street
             </div>
         </div>
 
@@ -27,13 +27,14 @@
     </div>
 
     <!-- Controls -->
-    <a class="left carousel-control" href="#" data-slide="prev">
-        <span class="icon-prev"></span>
-    </a>
-    <a class="right carousel-control" href="#" data-slide="next">
-        <span class="icon-next"></span>
-    </a>
+<!--    <a class="left carousel-control" href="#" data-slide="prev">-->
+<!--        <span class="icon-prev"></span>-->
+<!--    </a>-->
+<!--    <a class="right carousel-control" href="#" data-slide="next">-->
+<!--        <span class="icon-next"></span>-->
+<!--    </a>-->
 </div>
 
     </div>
+
 </div>
