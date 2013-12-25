253e3d04994211365c9b223fbbaacdfa5f25e58c
Fahad Kassim <fahad@yoteyote.com>
2013-12-25 16:05:43 +0200

    MERGE
    Add Files , Commit them via SHELL

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <?php include('usuals/header.php');
 $page_id = "home";
 include('usuals/menu.php');
-include('modules/login.php');
-    ?>
+include('modules/login.php');?>
 <?php //include('usuals/search.php');?>
-
 <?php include('usuals/footer.php'); ?>
