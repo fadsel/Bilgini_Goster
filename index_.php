d5bd86bcb44ffd4703106665e07808249e8e06bb
Fahad Kassim <fahad@yoteyote.com>
2013-12-25 16:02:17 +0200

    Cleaned up some errors at SHELL
    Merged some things ,

--- index_.php
+++ index_.php
@@ -3,6 +3,8 @@ $page_id = "home";
 include('usuals/menu.php');
 include('modules/login.php');
     ?>
-<?php //include('usuals/search.php');?>
+
+
+<?php include('usuals/search.php');?>
 
 <?php include('usuals/footer.php'); ?>
