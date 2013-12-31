d5ce3ea70aa49790998729ada847ea7b6e905f96
Fahad Kassim <fahad@yoteyote.com>
2013-12-31 16:16:59 +0200

    Coding for More Info
    On Plagiarism

--- modules/userContent.php
+++ modules/userContent.php
@@ -16,6 +16,36 @@
     <div class="row">
         <img style="border: 1px solid #ae4928;" src="./img/test/cover.jpg" />
     </div><br>
+    <div class="row">
+        <h1>Pragiarism</h1>
+        <hr>
+        <div class="row"><div class=""><h2>What is it?</h2>This part lets you compare files and see if their is any relationship in them i.e %Copy</div></div>
+        <div class="row">
+            <div class="bil">
+                <div class="row">
+                    <div class="col-lg-2"><img src="./img/test/profile_dummy.png"
+                                               class="img-thumbnail   img-responsive">
+                    </div>
+                    <div class="col-lg-8" style="margin-bottom: 15px">
+                        <b>Client : </b> Fahad
+                        <br><b>Document Name : </b> rapor.docx
+                        <div class="row more">
+                                <b>Word Count : </b> 23
+                                <br><b>Keywords : </b>45656
+                                <br><b>Language : </b> C++
+                                <br><b>Algorithm : </b> B+ Trees
+                                <br><b>Document Type : </b> docx
+                                <br><b>Document Size : </b> 502 kb
+                            </div>
+
+                    </div>
+                    <div class="col-lg-2 marks">
+                        73%
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
 
 <br>
